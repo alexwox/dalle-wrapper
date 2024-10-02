ccf16fc53a762c2bf7f756bfaabae228c46651f8
alexwox <alexander.woxstrom@gmail.com>
2024-10-02 20:12:55 +0200

    auth0 integration

---
+++ src/app/api/auth/[auth0]/route.js
@@ -0,0 +1,12 @@
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+
+export default handleAuth({
+    login: handleLogin(
+        {
+            authorizationParams: {
+                audience: "process.env.AUTH0_AUDIENCE",
+                scope: "openid email",
+            },
+        }
+    ),
+});
